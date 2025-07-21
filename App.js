312078fc998dfee69475667ac6c2908124351a94
Sujeet Kumar <38582345+sujeet712@users.noreply.github.com>
2025-07-21 21:13:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Body from "./components/Body";
+import Head from "./components/Head";
+function App() {
+  return (
+    <div>
+      <Head />
+      <Body />
+      {/*
+  Head
+  Body
+    SideBar
+      MenuItems
+ 
+       */}
+    </div>
+  );
+}
+
+export default App;
